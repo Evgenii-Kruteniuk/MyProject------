81a8448b3f6e6362a928f70abdd8e3490689fad9
Evgenii-Kruteniuk <muster00100@yandex.ru>
2023-03-01 15:17:12 +0300

    1/03/23

--- index.js
+++ index.js
@@ -2,45 +2,41 @@ const list = [
   {
     name: "butter",
     amount: 2,
-    price: 90,
-
-    func1() {
-      let butter = this.amount * this.price;
-      return butter;
-    },
+    bay: true,
   },
   {
     name: "milk",
     amount: 3,
-    price: 60,
-
-    func2() {
-      let milk = this.amount * this.price;
-      return milk;
-    },
+    bay: false,
   },
   {
     name: "bread",
     amount: 6,
-    price: 25,
-
-    func3() {
-      let bread = this.amount * this.price;
-      return bread;
-    },
+    bay: true,
+  },
+  {
+    name: "juice",
+    amount: 10,
+    bay: false,
   },
 ];
 
-console.log(list[0].func1());
-console.log(list[1].func2());
-console.log(list[2].func3());
-
-function totalSum() {
-  const sum = list[0].func1() + list[1].func2() + list[2].func3();
-  return sum;
+let arr = [];
+for (elem of list) {
+  if (elem.bay === false) {
+    arr.unshift(elem);
+  } else {
+    arr.push(elem);
+  }
 }
-alert(totalSum());
 
-const totalPrice = [list[0].func1(), list[1].func2(), list[2].func3()];
-const maxEl = Math.max(...totalPrice);
-console.log(maxEl);
+console.log(arr);
+
+function returnProduct(unit) {
+  for (product of arr) {
+    if (unit === product.name) {
+      console.log("Товар уже куплен");
+    }
+  }
+}
+returnProduct("butter");
